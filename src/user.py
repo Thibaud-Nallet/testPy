ce91b9f04e047b69dfc99c1aebf70112c1900230
Thibaud NALLET <Thibaud.NALLET@maroquineriethomas.com>
2021-02-05 15:51:04 +0100

    Ajout des assertions plus écritures des 2 tests par choix

--- src/user.py
+++ src/user.py
@@ -1,5 +1,12 @@
 class User:
     def __init__(self, name, firstname, annee):
+        assert name.isalnum(), "Attribut 'name' : doit être alphanumérique (a-z, A-Z et 0-9)"
+        assert len(name) > 3 and len(name) < 25, "Attribut 'name' : nombre de caractères compris entre 3 et 25"
+        assert firstname.isalnum(), "Attribut 'firstname' : doit être alphanumérique (a-z, A-Z et 0-9)"
+        assert len(firstname) > 3 and len(firstname) < 25, "Attribut 'firstname' : nombre de caractères compris entre 3 et 25"
+        assert isinstance(annee, int), "Attribut 'annee' : doit être un entier"
+        assert annee > 1900 and annee < 2021, "Attribut 'annee' : doit être compris entre 1900 et 2021"
+        
         self.name = name
         self.firstname = firstname
         self.annee = annee
--- tests/testuser.py
+++ tests/testuser.py
@@ -4,13 +4,39 @@ from src.user import User
 import unittest
 
 class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.p = User("nallet", "thibaud", 1991)
+
+    #verif création d'un user
     def test_user_is_created(self):
-        p = User("nallet", "thibaud", 1991)
-        self.assertIsInstance(p, User)
+        self.assertIsInstance(self.p, User)
+
+    #name 
+    def test_name_is_str(self): 
+        self.assertIsInstance(self.p.name, str)
     
+    def test_name_is_short(self): 
+        self.assertGreater(len(self.p.name), 0)
+
+    def test_name_is_long(self): 
+        self.assertLess(len(self.p.name), 25)
+
+    #firstname 
+    def test_firstname_is_str(self): 
+        self.assertIsInstance(self.p.firstname, str)
+    
+    def test_firstname_is_short(self): 
+        self.assertGreater(len(self.p.firstname), 0)
+
+    def test_firstname_is_long(self): 
+        self.assertLess(len(self.p.firstname), 25)
+
+    #Annnée
     def test_annee_is_positive(self): 
-        p = User("nallet", "thibaud", 1991)
-        self.assertGreater(p.annee, 0)
+        self.assertGreater(self.p.annee, 0)
+
+    def test_annee_is_int(self): 
+        self.assertIsInstance(self.p.annee, int)
 
 if __name__ == '__main__': 
     unittest.main()
